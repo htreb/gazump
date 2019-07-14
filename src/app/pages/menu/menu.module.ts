57255f02818913db637c1ce6a46d10744b494195
harry <harrytreble@hotmail.co.uk>
2019-07-14 16:24:45 +0700

    Building out app structure:  Creating log in page,  chat and board pages and side menu

---
+++ src/app/pages/menu/menu.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { MenuPage } from './menu.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MenuPage,
+    children: [
+      {
+        path: 'board',
+        loadChildren: '../board/board.module#BoardPageModule'
+      },
+      {
+        path: 'chat',
+        loadChildren: '../chat/chat.module#ChatPageModule'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [MenuPage]
+})
+export class MenuPageModule {}
