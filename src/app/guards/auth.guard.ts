27e2bcf7028cca73f07d2f58d968c2d7cb2cf5a2
harry <harrytreble@hotmail.co.uk>
2019-07-17 12:47:52 +0700

    Commented and cleaned up auth guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -8,7 +8,10 @@ import { take, map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-
+  /**
+   * This flag is picked up by the app component and is used to display the page-loading-spinner
+   * while the auth check is going on
+   */
   public loading = false;
 
   constructor(private router: Router, private auth: AuthService) {}
@@ -19,13 +22,13 @@ export class AuthGuard implements CanActivate {
     return this.auth.user.pipe(
       take(1),
       map(user => {
-          this.loading = false;
-          if (!user || expectedRoles.indexOf(user.role) < 0) {
-              this.router.navigateByUrl('/login');
-              return false;
-            }
-          return true;
-          })
-        );
-      }
+        this.loading = false;
+        if (!user || expectedRoles.indexOf(user.role) < 0) {
+          this.router.navigateByUrl('/login');
+          return false;
+        }
+        return true;
+      })
+    );
+  }
 }
