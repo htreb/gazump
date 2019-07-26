8a1d8e0a50ef0257bd92150ebd381c8347b16be4
harry <harrytreble@hotmail.co.uk>
2019-07-26 15:46:32 +0700

    filling out chat page to send and receive messages

--- src/app/pages/chat/chat.page.ts
+++ src/app/pages/chat/chat.page.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { ChatService } from 'src/app/services/chat.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { IonContent } from '@ionic/angular';
+import { map, tap } from 'rxjs/operators';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-chat',
@@ -7,9 +13,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatPage implements OnInit {
 
-  constructor() { }
+  chat;
+  message: string;
+  messages: Observable<any>;
+  currentUserId = this.auth.currentUser.value.id;
+
+  @ViewChild(IonContent) content: IonContent;
+  @ViewChild('input', { read: ElementRef }) inputContainer: ElementRef;
+
+  constructor(
+    private route: ActivatedRoute,
+    private chatService: ChatService,
+    private auth: AuthService,
+    private router: Router,
+    ) { }
 
   ngOnInit() {
+    this.route.params.subscribe(routeData => {
+      this.chatService.getOneChat(routeData.id).subscribe(chatData => {
+        this.chat = chatData;
+        this.messages = this.chatService.getChatMessages(this.chat.id).pipe(
+          map((messages: any) => {
+            for (const msg of messages) {
+              msg.user = this.getChatUser(msg.from);
+            }
+            return messages;
+          }),
+          tap(() => {
+            setTimeout(() => {
+              this.scrollToBottom(0);
+            }, 500);
+          })
+          );
+        });
+    });
   }
 
+  getChatUser(userId) {
+    for (const usr of this.chat.users) {
+      if (usr.id === userId) {
+        return usr.nickname;
+      }
+    }
+    return 'Deleted';
+  }
+
+
+  scrollToBottom(duration = 250) {
+    this.content.scrollToBottom(duration);
+  }
+
+  async sendMessage() {
+    const msg = this.message; // save the message so can reload it if it fails to send
+    this.message = ''; // clear message straight away to keep app responsive
+
+    try {
+      await this.chatService.addChatMessage(msg, this.chat.id);
+      this.scrollToBottom();
+    } catch (err) {
+      console.log('oh no!', err);
+      this.message = msg;
+    }
+  }
+
+  leaveChat() {
+    const newUsers = this.chat.users.filter(usr => usr.id !== this.currentUserId);
+
+    this.chatService.leaveChat(this.chat.id, newUsers).subscribe(res => {
+      this.router.navigateByUrl('/menu/chats');
+    });
+  }
 }
