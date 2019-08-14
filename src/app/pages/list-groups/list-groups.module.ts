fe20704c3cb71302a9b7dcfa44afa1a9d1f3205e
harry <harrytreble@hotmail.co.uk>
2019-08-14 14:52:27 +0800

    changing navaParam id => groupId

--- src/app/pages/list-groups/list-groups.module.ts
+++ src/app/pages/list-groups/list-groups.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-
 import { IonicModule } from '@ionic/angular';
-
 import { ListGroupsPage } from './list-groups.page';
 
 const routes: Routes = [
@@ -15,6 +13,10 @@ const routes: Routes = [
   {
     path: 'create',
     loadChildren: () => import('../create-group/create-group.module').then(m => m.CreateGroupPageModule)
+  },
+  {
+    path: ':groupId',
+    loadChildren: () => import('../menu/menu.module').then(m => m.MenuPageModule),
   },
 ];
 
