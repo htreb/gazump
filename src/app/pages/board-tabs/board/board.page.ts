af0a06e10eed2f27dc8c2bacd03da0df60980d60
harry <harrytreble@hotmail.co.uk>
2019-08-30 12:22:36 +0800

    implementing smooth-dnd on board page, needs debouncing

--- src/app/pages/board-tabs/board/board.page.ts
+++ src/app/pages/board-tabs/board/board.page.ts
@@ -5,26 +5,59 @@ import { BoardService } from 'src/app/services/board.service';
 @Component({
   selector: 'app-board',
   templateUrl: './board.page.html',
-  styleUrls: ['./board.page.scss'],
+  styleUrls: ['./board.page.scss']
 })
 export class BoardPage implements OnInit {
-
   board$;
   private boardId: string;
+  private newTicketSequence = {};
 
   constructor(
     private route: ActivatedRoute,
-    private boardService: BoardService) { }
+    private boardService: BoardService
+  ) {}
 
   ngOnInit() {
     // I shouldn't need to save this if it's coming down with the board
     this.boardId = this.route.snapshot.paramMap.get('boardId');
-
-    console.log(`${this.boardId} init`);
     this.board$ = this.boardService.boardsFromCurrentGroup(this.boardId);
-    console.log('the board is now', this.board$);
   }
 
+  onTicketDrop(column, tickets, dropResult) {
+    if (dropResult.removedIndex !== null || dropResult.addedIndex !== null) {
+      const newTickets = this.applyDrag(tickets, dropResult);
+      this.newTicketSequence[`tickets.${column.state}`] = newTickets;
+      this.updateDbTickets();
+    }
+  }
+
+  getTicketPayload(tickets) {
+    return index => {
+      return tickets[index];
+    };
+  }
+
+  applyDrag(arr, dragResult) {
+    const { removedIndex, addedIndex, payload } = dragResult;
+    if (removedIndex === null && addedIndex === null) {
+      return arr;
+    }
+    if (removedIndex !== null) {
+      arr.splice(removedIndex, 1);
+    }
+    if (addedIndex !== null) {
+      arr.splice(addedIndex, 0, payload);
+    }
+    return arr;
+  }
+
+  updateDbTickets() {
+    // TODO DEBOUNCE THIS!
+    return this.boardService.updateBoard(this.boardId, this.newTicketSequence).then(() => {
+      console.log('updated db tickets were', this.newTicketSequence);
+      this.newTicketSequence = {};
+    });
+  }
 
 
 
