feec1d98e58565aca897d05073ae91050a330674
harry <harrytreble@hotmail.co.uk>
2019-09-13 14:42:38 +0800

    only close tip when clicking on the blocker or resizing window. mouseover just gets annoying

--- src/app/shared/tip/tip.component.ts
+++ src/app/shared/tip/tip.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, HostListener } from '@angular/core';
 import { TextComponent } from '../text/text.component';
 import { PopoverController } from '@ionic/angular';
-
 @Component({
   selector: 'app-tip',
   templateUrl: './tip.component.html',
@@ -12,14 +11,10 @@ export class TipComponent {
   @Input() text;
   @Input() colour;
   private tip: HTMLIonPopoverElement;
-  private closeTipTimeout;
-  private mouseMovedSinceLastShow = true;
 
   constructor(private popoverController: PopoverController) { }
 
-  async showTip(ev: any, fromMouseOver = false) {
-    if (fromMouseOver && !this.mouseMovedSinceLastShow) { return; }
-    clearTimeout(this.closeTipTimeout);
+  async showTip(ev: any) {
     this.tip = await this.popoverController.create({
       component: TextComponent,
       event: ev,
@@ -30,20 +25,13 @@ export class TipComponent {
         colour: this.colour,
       }
     });
-    this.closeTipTimeout = setTimeout(_ => this.closeTip(), 3000);
     return await this.tip.present();
-  }
-  mouseMoved() {
-    this.mouseMovedSinceLastShow = true;
   }
 
   @HostListener('window:resize', ['$event'])
   closeTip(ev: any = null) {
-    clearTimeout(this.closeTipTimeout);
     if (this.tip) {
       this.tip.dismiss();
     }
-    this.mouseMovedSinceLastShow = false;
   }
-
 }
