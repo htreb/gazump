6b60354ec5d5eb1101b4fa8acbdf603772765cbe
harry <harrytreble@hotmail.co.uk>
2019-09-24 14:06:38 +0800

    Members pipe gets contacts from user object in database now, no need to duplicate contact details everywhere

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -12,18 +12,36 @@ export class ContactService {
     return {
       id: this.auth.currentUser.value.id,
       userName: this.auth.currentUser.value.userName,
-      email: this.auth.currentUser.value.email,
+      email: this.auth.currentUser.value.email
     };
   }
 
   getUsersContacts() {
     return this.auth.currentUser.pipe(
       map(userDoc => {
-        return Object.keys(userDoc.connections).map(id => { return {
-          id,
-          ...userDoc.connections[id]
-        }; });
+        return Object.keys(userDoc.connections).map(id => {
+          return {
+            id,
+            ...userDoc.connections[id]
+          };
+        });
       })
     );
+  }
+
+  getDetailsFromId(id: string) {
+    if (
+      this.auth.currentUser.value &&
+      this.auth.currentUser.value.connections &&
+      this.auth.currentUser.value.connections[id]
+    ) {
+      return this.auth.currentUser.value.connections[id];
+    } else if (id === this.auth.currentUser.value.id) {
+      return {
+        userName: 'You',
+        email: this.auth.currentUser.value.email,
+      };
+    }
+    return { userName: 'Unknown', email: 'Unknown' };
   }
 }
--- src/app/shared/pipes/members-detail.pipe.ts
+++ src/app/shared/pipes/members-detail.pipe.ts
@@ -1,14 +1,31 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Pipe({
   name: 'membersDetail'
 })
 export class MembersDetailPipe implements PipeTransform {
-  transform(members: any): any {
-    const memberNames = Object.values(members).map((member: any) => member.userName);
+  constructor(private contactService: ContactService) {}
+
+  transform(memberIds: any): any {
+    const memberNames = Object.keys(memberIds).map(
+      id => this.contactService.getDetailsFromId(id).userName
+    );
+    memberNames.sort((a, b) => {
+      if (a === 'Unknown') {
+        return 1;
+      }
+      if (b === 'Unknown') {
+        return -1;
+      }
+      return a < b ? -1 : 1;
+    });
     if (memberNames.length < 4) {
-      return `${memberNames.join(', ')}.`;
+      return `${memberNames.slice(0, memberNames.length - 1).join(', ')} and ${
+        memberNames[memberNames.length - 1]
+      }.`;
     }
-    return `${memberNames.slice(0, 2).join(', ')} and ${memberNames.length - 2} others.`;
+    return `${memberNames.slice(0, 2).join(', ')} and ${memberNames.length -
+      2} others.`;
   }
 }
