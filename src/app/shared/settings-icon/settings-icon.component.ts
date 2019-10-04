9b370a8cdbb00ea0411a76c3f2edb4cb2880a87e
harry <harrytreble@hotmail.co.uk>
2019-10-04 19:32:46 +0800

    close the settings if the options have changed

--- src/app/shared/settings-icon/settings-icon.component.ts
+++ src/app/shared/settings-icon/settings-icon.component.ts
@@ -1,20 +1,34 @@
-import { Component, HostListener, Input } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  Input,
+  OnChanges,
+  SimpleChanges
+} from '@angular/core';
 import { PopoverController } from '@ionic/angular';
 import { SettingsListComponent } from '../settings-list/settings-list.component';
 
 @Component({
   selector: 'app-settings-icon',
   templateUrl: './settings-icon.component.html',
-  styleUrls: ['./settings-icon.component.scss'],
+  styleUrls: ['./settings-icon.component.scss']
 })
-export class SettingsIconComponent {
-
+export class SettingsIconComponent implements OnChanges {
   @Input() settingsOptions;
   private popover: HTMLIonPopoverElement;
 
-  constructor(
-    private popoverController: PopoverController,
-  ) { }
+  constructor(private popoverController: PopoverController) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // close the settings if the options have changed.
+    if (
+      changes.settingsOptions && changes.settingsOptions.previousValue &&
+      changes.settingsOptions.currentValue.length !==
+      changes.settingsOptions.previousValue.length
+    ) {
+      this.closeSettings();
+    }
+  }
 
   async showSettings(ev: any) {
     this.popover = await this.popoverController.create({
@@ -22,18 +36,16 @@ export class SettingsIconComponent {
       event: ev,
       componentProps: {
         closePopover: this.closeSettings,
-        settingsOptions: this.settingsOptions,
+        settingsOptions: this.settingsOptions
       }
     });
     return await this.popover.present();
   }
 
   @HostListener('window:resize', ['$event'])
-  closeSettings(ev: any) {
+  closeSettings(ev?: any) {
     if (this.popover) {
       this.popover.dismiss();
     }
   }
-
-
 }
