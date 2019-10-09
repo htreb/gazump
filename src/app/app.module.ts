97b3cff728c7e93f0e31c728680979db2b93b5dd
harry <harrytreble@hotmail.co.uk>
2019-10-09 13:05:26 +0800

    Deleting page loading spinner component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from 'src/environments/environment';
-import { PageLoadingSpinnerComponent } from './page-loading-spinner/page-loading-spinner.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { IonicStorageModule } from '@ionic/storage';
@@ -23,7 +22,7 @@ import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
-  declarations: [AppComponent, PageLoadingSpinnerComponent],
+  declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
