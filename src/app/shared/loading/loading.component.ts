c06fdc9129ed32b02dc704a6c4e569d6960ae58c
harry <harrytreble@hotmail.co.uk>
2019-10-09 13:01:17 +0800

    Animate loading component in and out

--- src/app/shared/loading/loading.component.ts
+++ src/app/shared/loading/loading.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 @Component({
   selector: 'app-loading',
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      state('*', style({ opacity: 1 })),
+      state('void', style({ opacity: 0 })),
+      transition('void => *', [animate('0.6s 0.0s ease-out')]),
+      transition('* => void', [animate('0.6s 0.0s ease-in')])
+    ])
+  ]
 })
 export class LoadingComponent {
 
+  @Input() show = true;
   constructor() { }
-
 }
