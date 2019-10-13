2fd838309a17916a5ed0818fd658fbaaa912d8da
harry <harrytreble@hotmail.co.uk>
2019-10-13 09:29:01 +0800

    need prod firebase to build prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCauP8HlLkYCnonHNRyKUf2X6-nWxpIDFM',
+    authDomain: 'gazump-46a24.firebaseapp.com',
+    databaseURL: 'https://gazump-46a24.firebaseio.com',
+    projectId: 'gazump-46a24',
+    storageBucket: 'gazump-46a24.appspot.com',
+    messagingSenderId: '602030666254',
+    appId: '1:602030666254:web:892d128fea5491d7'
+  }
 };
