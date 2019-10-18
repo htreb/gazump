39a90a985f04c6fb610683d2cc99bd57886d5a23
harry <harrytreble@hotmail.co.uk>
2019-10-18 21:16:39 +0800

    actually exporting the fcm functions in index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,2 +1,3 @@
 // export { userNameChanged } from './user-name-changed';
 export { completeContactRequest } from './contact-request';
+export { subscribeToTopic, unsubscribeFromTopic, sendOnFirestoreCreate } from './fcm';
